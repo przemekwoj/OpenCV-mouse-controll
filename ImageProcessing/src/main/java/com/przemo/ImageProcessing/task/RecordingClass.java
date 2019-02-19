5d386ba6e6bdb5b30ee6b4d1bca45d95ac583e0e
przemekwoj <przemoemo@poczta.onet.pl>
2019-02-20 00:50:03 +0100

    recognizint just my circle on a screen

--- ImageProcessing/src/main/java/com/przemo/ImageProcessing/task/RecordingClass.java
+++ ImageProcessing/src/main/java/com/przemo/ImageProcessing/task/RecordingClass.java
@@ -3,8 +3,16 @@ package com.przemo.ImageProcessing.task;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
@@ -13,6 +21,7 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 
+import static org.opencv.photo.Photo.fastNlMeansDenoising;
 
 public class RecordingClass extends Task<Void>
 {
@@ -31,6 +40,11 @@ public class RecordingClass extends Task<Void>
     private DataBufferByte dataBuffer;
     
     private byte[] data;
+    
+    private List<MatOfPoint> contours;
+    private List<MatOfPoint2f> approxCurves;
+    
+    private Mat hierarchy;
     
     public RecordingClass() {}
     
@@ -61,10 +75,33 @@ public class RecordingClass extends Task<Void>
     		Thread.sleep(33);
     		// Reading the next video frame from the camera 
     		getFrame();
+    		//gaussian blurring
+    		//Imgproc.GaussianBlur(matrix, matrix, new Size(7,7),0);
         	//convert color image to grey image
     		convertColor(matrix);
+    		//dilation white color are more prominent
+    		Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(13,13));
+     		Imgproc.dilate(matrix, matrix, kernel);
+    		//edrode - oppisite to dilation
+     		kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(13,13));
+      		Imgproc.erode(matrix, matrix, kernel);
+      		//gaussian blurring
+    		Imgproc.GaussianBlur(matrix, matrix, new Size(45,45),0);
+    		Core.inRange(matrix, new Scalar(85), new Scalar(95), matrix);
+    		//to zero
+    		//Imgproc.threshold(matrix, matrix, 110, 255, Imgproc.THRESH_TOZERO);
+    		//grey image fast denoise
+    		//denoiseImage(matrix);
+    		//grey to binary, just to make image more simple , less detail
+    		//grey_to_binary(matrix);
     		//edge detection
     		edgeDetection(matrix);
+    		//findContours
+    		findContorus(matrix);
+    		//circle detecting
+    		detectingCircle();
+    		//drawContorus
+    		Imgproc.drawContours(matrix, contours, -1, new Scalar(255), -1);
         	// Creating BuffredImage from the matrix
     		createBufferImage(matrix);
             // Creating the Writable Image
@@ -74,6 +111,91 @@ public class RecordingClass extends Task<Void>
     	}
 	}
 	
+	int i = 0;
+	public void detectingCircle()
+	{
+		MatOfPoint matOfPoint = new MatOfPoint();
+	    MatOfPoint2f approx = new MatOfPoint2f();
+	    approxCurves = new ArrayList<MatOfPoint2f>();
+	    System.out.println(contours.size() + " cntours size befor");
+	    for(int i = 0 ; i<contours.size();i++)
+	    {
+	    	if( Imgproc.contourArea(contours.get(i)) < 65)
+	    	{
+	    		contours.remove(i);
+	    		i--;
+	    	}
+	    }
+	    System.out.println(contours.size() + " cntours size after");
+	    
+	    int j = 0;
+	    List<Integer> toRemove = new ArrayList<Integer>();
+		for(MatOfPoint contour: contours)
+		{
+			double epsilon = 0.0001*Imgproc.arcLength(new MatOfPoint2f(contours.get(j).toArray()),true);
+		    Imgproc.approxPolyDP(new MatOfPoint2f(contours.get(j).toArray()),approx,epsilon,true);
+		    if(approx.width()*approx.height() < 150)
+		    {
+		    	toRemove.add(j);
+		    }
+		    j++;
+		}
+		
+		for(int i = 0 ,index = 0; i < toRemove.size() ; i++ )
+		{
+			contours.remove(toRemove.get(i) - index);
+			index++;
+		}
+		
+	}
+
+
+	public void filter2d() 
+	{
+		//create kernel , mask
+		Mat ones = Mat.ones( 3, 3, matrix.depth());		
+		int row = 0, col = 0;
+		for(int i = 0; i<ones.height(); i++)
+		{
+			for(int j = 0; j<ones.width(); j++)
+			{
+				ones.put(i, j, 1);
+			}
+		}
+		//2d filtering
+		Imgproc.filter2D(matrix, matrix, matrix.depth(), ones);
+	}
+
+
+	public void denoiseImage(Mat matrix) 
+	{
+		fastNlMeansDenoising(matrix,matrix);
+	}
+
+
+	public void grey_to_binary(Mat matrix) 
+	{
+		Imgproc.threshold(matrix, matrix, 30, 255, Imgproc.THRESH_BINARY);
+	}
+
+
+	public void findContorus(Mat matrix)
+	{
+		contours = new ArrayList<MatOfPoint>();
+		hierarchy = new Mat();
+		Imgproc.findContours(matrix, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+
+		
+		for(int i = 0; i<matrix.height(); i++)
+		{
+			for(int j = 0; j<matrix.width(); j++)
+			{
+				matrix.put(i, j, 0);
+			}
+		}
+	}
+
+
 	public void getFrame()
 	{
 		matrix = new Mat(); 
